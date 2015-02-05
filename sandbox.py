4f53fc3b98928589b42eb9c378eee8fa8af5f4c6
Ankur Aggarwal <aggarwalanku@gmail.com>
2015-02-05 06:13:20 +0530

    betting

--- sandbox.py
+++ sandbox.py
@@ -7,3 +7,45 @@ if __name__ == "__main__":
 	for x in d:
 		print [ str(t) for t in x ]
 		print c.PileScore( x )
+	bet=16
+	p1=1
+	p2=2
+	option_player2 = ''
+	while (bet<=29 and p2<5):
+			print "Bet=",bet
+			print "Player",p1
+			print option_player2
+			if (option_player2 != '3'):
+				if (bet!=16):
+					option=raw_input("Enter your options :1.Bet 2.Stay 3.Pass")
+				else:
+					option=raw_input("Enter your options :1.Bet 2.pass")	
+				print option
+				if (option=='1' and option_player2 != '3'):
+					bet1 = int(raw_input("Enter your bet : "))
+					if (bet1<=bet):
+						print "Invalid Input"
+						continue
+					else:
+						bet=bet1	
+				elif ((option == '3' and bet != 16 ) or (option == '2' and bet == 16)):
+					p1,p2 = p2,p2+1	
+
+					
+			print "Player",p2
+			option_player2=raw_input("Enter your options :1.Bet 3.Pass")
+			if (option_player2 == '1'):
+				bet_player2 = int(raw_input("Enter your bet player2:"))
+				if(bet_player2<=bet):
+					print "Invalid Input"
+				else:
+					bet = bet_player2	
+			else:
+				p2=p2+1
+	print bet,p1
+
+
+
+
+
+
